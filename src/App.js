d918663e91d9b3ba91a96df3af211c1730dd5786
chakkasailesh <chakkasailesh444@gmail.com>
2024-01-28 23:35:02 +0530

    added home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import Login from './components/Login'
+import Upload from './components/Upload'
+import { Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
-      <Login />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Upload />} />
+      </Routes>
     </div>
   )
 }
